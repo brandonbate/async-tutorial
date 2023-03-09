fc5afd01257e3228bcb1bb5676d89f14b6b60d67
brandonbate <41764599+brandonbate@users.noreply.github.com>
2023-03-09 12:15:33 -0500

    Update example4.py

--- example4.py
+++ example4.py
@@ -11,7 +11,4 @@ async def main():
     await asyncio.gather(count(), count(), count())
 
 if __name__ == "__main__":
-    s = time.perf_counter()
     asyncio.run(main())
-    elapsed = time.perf_counter() - s
-    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
